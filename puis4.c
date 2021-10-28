3c3f9db7dc7b9546abb7d89e2adccde4cb2aa104
Riseik <emaillard@gaming.tech>
2021-10-28 18:16:54 +0200

    Program final version

--- puis4.c
+++ puis4.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char grid[6][9];
-int line;
-int column;
-int can_be_placed;
-int place;
-int victory;
-int play_again = 1;
+char grid[6][9];     //Initialization of the size of the grid.
 
-void basic_grid()
+int line;     //Variable that will count the lines.
+int column;     //Variable that will count the columns.
+int victory;     //Variable that will be remember if the game is finished or not.
+int play_again = 1;     //Variable that will remember if the players want to play or not.
+
+
+void basic_grid()     //Function for the basic aspect and the reset of the grid.
 {
-  printf("  1 2 3 4 5 6 7 \n");
-  printf("_________________\n");
+  printf("  1 2 3 4 5 6 7 \n");     //There will be a nmber for each column.
+  printf("_________________\n");     //There will be "_" above and below the grid.
   for (line = 0; line < 6; line++)
   {
     for (column = 0; column < 9; column++)
     {
       if (column == 0 || column == 8)
       {
-        grid[line][column] = '|';
+        grid[line][column] = '|';     //The sides will be delimited by "|".
       }
       else
       {
-        grid[line][column] = '.';
+        grid[line][column] = '.';     //The grid will be full of ".".
       }
       printf("%c", grid[line][column]);
       printf(" ");
@@ -33,7 +33,7 @@ void basic_grid()
   printf("_________________\n");
 }
 
-void game_grid()
+void game_grid()     //Function for the grid with the placed tokens.
 {
   printf("  1 2 3 4 5 6 7 \n");
   printf("_________________\n");
@@ -49,7 +49,7 @@ void game_grid()
       {
         if (grid[line][column] == 'X')
         {
-          printf("\e[1;34m%c\e[0m", grid[line][column]);
+          printf("\e[1;34m%c\e[0m", grid[line][column]);     //\e are for colouring the tokens.
         }
         else
         {
@@ -58,6 +58,8 @@ void game_grid()
         printf(" ");
         continue;
       }
+      //"If" above put the placed token in the grid.
+
       else
       {
         grid[line][column] = '.';
@@ -70,7 +72,7 @@ void game_grid()
   printf("_________________\n");
 }
 
-void test_victory(player)
+void test_victory(int player)     //Function for the test of a winner, which means 4 same tokens in a row.
 {
   for (line = 0; line < 6; line++)
   {
@@ -82,6 +84,8 @@ void test_victory(player)
       }
     }
   }
+  //"For" above analyze the horizontal.
+
   for (line = 0; line < 4; line++)
   {
     for (column = 1; column < 5; column++)
@@ -92,6 +96,8 @@ void test_victory(player)
       }
     }
   }
+  //"For" above analyze the diagonal down-right.
+
   for (line = 0; line < 4; line++)
   {
     for (column = 1; column < 8; column++)
@@ -102,6 +108,8 @@ void test_victory(player)
       }
     }
   }
+  //"For" above analyze the vertical.
+
   for (line = 0; line < 4; line++)
   {
     for (column = 3; column < 8; column++)
@@ -112,6 +120,8 @@ void test_victory(player)
       }
     }
   }
+  //"For" above analyze the down-left.
+
   if (victory == 1)
   {
     if (player == 0)
@@ -123,9 +133,10 @@ void test_victory(player)
       printf("Well done Player 2 (\e[1;31mO\e[0m), you've won! ");
     }
   }
+  //"If" above congratulate the winner.
 }
 
-void test_draw()
+void test_draw()     //Function for the test of a full grid, which means a draw.
 {
   int draw = 1;
   for (line = 0; line < 6; line++)
@@ -145,9 +156,10 @@ void test_draw()
   }
 }
 
-void placed_token(player, token)
+void placed_token(int player, int token)     //Function that ask where to place the token and place it in the grid.
 {
-  can_be_placed = 0;
+  int place;     //Variable that remember the chosen column by the player.
+  int can_be_placed = 0;     //Variable that remember if the token can be placed.
   while (can_be_placed == 0)
   {
     if (player == 0)
@@ -176,6 +188,7 @@ void placed_token(player, token)
         }
         break;
       }
+      //"If" above analyze if the layer is surrendering or not.
       else if (place < 1 || place > 7)
       {
         printf("Invalid column...");
@@ -185,6 +198,8 @@ void placed_token(player, token)
       {
         continue;
       }
+      //"If" above analyze if the token is the lowest possible.
+
       else if (grid[line][place] == '.' && grid[line + 1][place] != '.')
       {
         grid[line][place] = token;
@@ -200,10 +215,10 @@ void placed_token(player, token)
   }
 }
 
-void another_game()
+void another_game()     //Function that ask for another game.
 {
-  char answer;
-  int decision = 0;
+  char answer;     //Variable that remember the decision of the player to whether replay a game or not.
+  int decision = 0;     //Variable that remember if the player has make a decision.
   while (decision == 0)
   {
     printf("Wanna play another game? (\e[1;32my\e[0m/\e[1;31mn\e[0m)\n");
@@ -213,7 +228,7 @@ void another_game()
     {
       play_again = 0;
       decision = 1;
-      printf("Good bye!");
+      printf("Good bye!\n");
     }
     else if (answer == 'y')
     {
@@ -229,13 +244,13 @@ void another_game()
 
 void main()
 {
-  char token;
+  char token;     //Variable that remember which token is actually played.
   play_again = 1;
   while (play_again == 1)
   {
-    int draw = 0;
+    int draw = 0;     //Variable that remember if the grid is full or not.
     victory = 0;
-    int player = 0;
+    int player = 0;     //Variable that remember which player is playing.
     printf("Player 1 play with \e[1;34mX\e[0m and player 2 play with \e[1;31mO\e[0m.\n\nWrite (-1) to forfeit.\n");
     basic_grid();
     while (victory == 0)
@@ -248,6 +263,8 @@ void main()
       {
         token = 'O';
       }
+      //"If" above switch the token.
+
       placed_token(player, token);
       if (victory == 0)
       {
@@ -266,6 +283,8 @@ void main()
       {
         player = 0;
       }
+      //"If" above switch the player.
+
     }
     another_game();
   }
